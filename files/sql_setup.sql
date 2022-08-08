618fa3e0e52ebda04a49fcdf40906abfbaa55ffd
Siebren Kraak <siebrenkraak@hotmail.com>
2022-08-08 11:57:23 +0200

    Fix SQL error

--- files/sql_setup.sql
+++ files/sql_setup.sql
@@ -1,11 +1,11 @@
 USE securavulnerabledb
 GO
 CREATE TABLE vpn_employee_data (
-    username varchar(255),
-    password varchar(255)
+    vpn_username varchar(255),
+    vpn_password varchar(255)
 )
 GO
-INSERT INTO secura_flags VALUES ('Employee23187', 'SECURA{VPN_CR3D3NT14LS}')
+INSERT INTO vpn_employee_data VALUES ('Employee23187', 'SECURA{VPN_CR3D3NT14LS}')
 GO
 CREATE LOGIN DevOps WITH PASSWORD = 'SECURA{C0NN3CT10N_STR1NG}';
 CREATE USER DevOps FOR LOGIN DevOps;
